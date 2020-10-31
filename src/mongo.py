cb1a278ac42e94bf67a73f201f1f3d6a70343ab7
self-stack <53274146+zacharyksb@users.noreply.github.com>
2020-10-31 02:25:43 -0500

    mongo script init

---
+++ src/mongo.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+
+def connect_mongo():
+    global client
+    client = MongoClient('localhost', 27017)
+
+def connect_coll(db_name, coll_name):
+    global coll
+    db = client[db_name]
+    coll = db[coll_name]
+
+def insert_one(d):
+    coll.insert(d)
