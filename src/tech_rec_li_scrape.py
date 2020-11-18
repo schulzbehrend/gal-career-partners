f6e408e28191b73513c7b1ced2d76559ff979da5
self-stack <53274146+zacharyksb@users.noreply.github.com>
2020-11-18 11:57:54 -0600

    clean main add var import

--- src/tech_rec_li_scrape.py
+++ src/tech_rec_li_scrape.py
@@ -12,6 +12,7 @@ import chromedriver_binary
 from bs4 import BeautifulSoup
 
 import mongo
+from companies import cos_list
 
 def get_login():
     '''
@@ -176,41 +177,6 @@ def construct_record(results, co):
 
 
 if __name__ == '__main__':
-    # df = pd.read_csv('../data/glassdoor_scrape_1.csv')
-    # df.dropna(inplace=True)
-    # edu_flag = df['industry'].apply(lambda x: True if 'College' in x else False)
-    # cos = df['name']
-    # pd.Series(df.name[~edu_flag].unique())
-
-    cos_list = ['9Gauge Partners', 'Abbott', 'Aceable', 'AdbaKx', 'Addison Group', 'Aditi Consulting', 'Adroit Associates Inc', 
-    'Advanced Solutions International', 'Affinipay', 'AgileAssets', 'AbbVie', 'Greystar', 'Alpha Nodus', 
-    'American Campus Communities', 'Amplify', 'Applied Research Laboratories', 'ARM', 'Aspen Heights', 'Atrium', 'Aunt Bertha', 
-    'Aurigo Software Technologies', 'Avanade', 'Dun & Bradstreet', 'BAE Systems USA', 'Balyasny Asset Management', 'Kasasa', 
-    'BearingPoint', 'Bell', 'Bigcommerce', 'BlackLocus', 'Blue Cross Blue Shield', 'Boeing', 'Booz Allen Hamilton', 'Boundless', 
-    'BP', 'BuildASign.com', 'Calculated Hire', 'Carvana', 'Centerpoint Energy', 'Cerebri AI', 'CGI', 'Cigna', 'City of Austin', 
-    'ClearDATA', 'Client Network Services', 'LMI', 'Cloud BigData Technologies Group', 'Cloudera', 'Cocolevio', 
-    'Cognizant Technology Solutions', 'Compeat', 'ConnectTel', 'ConsultAdd', 'Cratejoy', 'CreditCards.com', 'CreditShop', 
-    'Crossover for Work', 'Cybersource', 'Deverus', 'Digital Turbine', 'Dow', 'Doximity', 'Dura Medic', 
-    'Dynamic Computing Services', 'E2OPEN', 'eMDs', 'Emerson', 'eMids Technologies', 'Emtec', 'EPC Group.net', 'Equinor', 
-    'Equity Methods', 'eSolutions (UAE)', 'Ethos Life', 'Everfest', 'eVestment', 'EY', 'FMTC', 'Ford Motor Company', 'Gartner', 
-    'GE Aviation', 'GitHub', 'GLG', 'Global Resources', 'Goldman Sachs', 'Hayes Software Systems', 'Hypergiant', 'Infogain', 
-    'InstaPay', 'Integra FEC', 'IRS', 'Itransition', 'JASK', 'Josh Technology Group', 'Jovio', 'KAnand', 'Kasasa', 'KNIME', 
-    'KUKA', 'KUNGFU.AI', 'Latitude 36', 'Lattice Engines', 'Marsh', 'McKinsey & Company', 'Merck', 'Meridian Technology Group', 
-    'MMC Systems', 'Morningstar', 'Mr. Cooper', 'Myriad Genetics', 'Netspend', 'NFP', 'NHRG', 'NIC', 'Nimbus Data', 'Nutrabolt', 
-    'ONE Gas', 'Optum', 'Outdoorsy', 'Palisades Holdings', 'PARAM HEALTHCARE & IT SERVICES, INC', 'Parker & Lynch', 
-    'Peak Performers', 'Pecan Street Insurance', 'Perficient', 'Ping Identity', 'Praecipio Consulting', 'Praxent', 
-    'Prelude Systems', 'PRO Unlimited', 'Pushnami', 'Pyramid Consulting', 'Qcue', 'Randstad US', 'Raybeam', 'RealMassive', 
-    'Rethink', 'Revionics', 'REX', 'Robert Half', 'Rockwell Automation', 'Ruffalo Noel Levitz', 'Saatva', 'Satin Solutions', 
-    'SD', 'Secureworks', 'Seilevel', 'Sense Corp', 'ShippingEasy', 'Shogun', 'Sierra Systems', 'Simple Government Solutions', 
-    'SMART Apartment Data', 'Softcorp International', 'SourcePulse', 'Southwest Airlines', 'SpeedTech Lights', 'Square Root', 
-    'Staffmark', 'Stocked Robotics', 'Synergy Global Technologies', 'Sysco Labs (SL)', 'System Soft Technologies', 'TASC', 
-    'Teacher Retirement System of Texas', 'Tech Data', 'Tecra Systems', 'Tenfold', 'Texas Comptroller of Public Accounts', 
-    'Texas Department of Insurance', 'Texas Legislative Council', 'The Home Depot', 'The Zebra', 'Tindall and Foster PC', 
-    'TrailStone', 'Trendline Interactive', 'Tresl', 'TRI Environmental', 'TRUST Consulting Group', 'TurnKey Vacation Rentals', 
-    'United Way', 'University Co-op', 'University Federal Credit Union', 'Valkyrie Intelligence', 'Vast', 'Velocity Electronics', 
-    'Viasat', 'Virtu Financial', 'Volt Consulting Group', 'Wargaming', 'Wentwood Capital Advisors', 'WePay', 'Workify', 
-    'Workplace Answers', 'XO Group', 'Xtivia', 'Z5 Inventory', 'Zilliant', 'Zuar']
-
     cos = pd.Series(cos_list)
 
     driver = li_login()
