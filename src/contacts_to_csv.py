934df1e79422aae59604998d9d77bd9552c66033
self-stack <53274146+zacharyksb@users.noreply.github.com>
2020-11-30 13:58:50 -0600

    add df construction

--- src/contacts_to_csv.py
+++ src/contacts_to_csv.py
@@ -6,10 +6,22 @@ import mongo
 def main():
     mongo.connect_mongo()
     mongo.connect_coll('gal_part_proj', 'ATX_tech_rec')
-    cursor = mongo.coll.find({{}, {'_id':0}})
+    cursor = mongo.coll.find({}, {'_id':0})
 
     df = pd.DataFrame(cursor)
     df = df.T
 
+    index = list()
+    results = list()
+
+    for idx, row in df.iterrows():
+        index.append(idx)
+        results.append(row[row.notnull()].values[0])
+
+    clean_df = pd.DataFrame(results, index=index)
+    clean_df.rename({0: 'Scrape Results'}, axis=1, inplace=True)
+    clean_df.head()
+
 
 if __name__ == '__main__':
+    main()
